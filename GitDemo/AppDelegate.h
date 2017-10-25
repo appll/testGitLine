95447f18b2646e052ac0fc36c4615b244eeb86ba
zhanglu <lu.zhang@tqmall.com>
2017-10-25 11:16:55 +0800

    init project

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by zhanglu on 25/10/2017.
+//  Copyright © 2017 zhanglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by zhanglu on 25/10/2017.
+//  Copyright © 2017 zhanglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
